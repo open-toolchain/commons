2e8001ac09c98e2778b118da0306255b0fef0dcf
Philippe Mulet <phmulet@philippes-mbp.pornichet.fr.ibm.com>
2018-02-19 12:45:44 +0100

    add space

--- scripts/check_prebuild.sh
+++ scripts/check_prebuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # uncomment to debug the script
-#set -x
+# set -x
 # copy the script below into your app code repo (e.g. ./scripts/check_prebuild.sh) and 'source' it from your pipeline job
 #    source ./scripts/check_prebuild.sh
 # alternatively, you can source it from online script:
