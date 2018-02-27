9de19a5d775e83c00147871a7613d55dad6e9a27
Philippe Mulet <phmulet@philippes-mbp.pornichet.fr.ibm.com>
2018-02-27 13:52:22 +0100

    fix extra param

--- scripts/publish_helm_package.sh
+++ scripts/publish_helm_package.sh
@@ -78,8 +78,7 @@ echo "PUBLISH CHART PACKAGE"
 # Refresh in case of concurrent updates
 git -C ./${UMBRELLA_REPO_NAME} pull --no-edit
 echo "Updating charts index"
-# touch ./${UMBRELLA_REPO_NAME}/charts/index.yaml
-helm repo index ./${UMBRELLA_REPO_NAME}/charts ./${UMBRELLA_REPO_NAME}/charts/index.yaml --url ${UMBRELLA_REPO_URL}/tree/master/charts
+helm repo index ./${UMBRELLA_REPO_NAME}/charts --url ${UMBRELLA_REPO_URL}/tree/master/charts
 
 cd ${UMBRELLA_REPO_NAME}
 git add .
