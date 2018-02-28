e6b98f5dbd4cd873261f73e4a98813c5d60926f3
Philippe Mulet <phmulet@philippes-mbp.pornichet.fr.ibm.com>
2018-02-28 12:48:16 +0100

    bind service to cluster

---
+++ scripts/cluster_bind_service.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# uncomment to debug the script
+#set -x
+# copy the script below into your app code repo (e.g. ./scripts/cluster_bind_service.sh) and 'source' it from your pipeline job
+#    source ./scripts/cluster_bind_service.sh
+# alternatively, you can source it from online script:
+#    source <(curl -sSL "https://raw.githubusercontent.com/open-toolchain/commons/master/scripts/cluster_bind_service.sh")
+# ------------------
+# source: https://raw.githubusercontent.com/open-toolchain/commons/master/scripts/cluster_bind_service.sh
+
+# This script does bind a given IBM Cloud Service identified by its SERVICE_ID to a cluster (via secret).
+# This script should be executed in a Kubernetes deploy job.
+
+#View build properties
+# cat build.properties
+# also run 'env' command to find all available env variables
+# or learn more about the available environment variables at:
+# https://console.bluemix.net/docs/services/ContinuousDelivery/pipeline_deploy_var.html#deliverypipeline_environment
+
+# Input env variables from pipeline job
+echo "PIPELINE_KUBERNETES_CLUSTER_NAME=${PIPELINE_KUBERNETES_CLUSTER_NAME}"
+echo "CLUSTER_NAMESPACE=${CLUSTER_NAMESPACE}"
+
+BINDING=$( bx cs cluster-services -n $CLUSTER_NAMESPACE $PIPELINE_KUBERNETES_CLUSTER_NAME | grep $SERVICE_ID ||:)
+if [ -z "$BINDING" ]; then
+  bx cs cluster-service-bind $PIPELINE_KUBERNETES_CLUSTER_NAME $CLUSTER_NAMESPACE $SERVICE_ID
+else
+  echo "Service already bound in cluster namespace"
+fi
