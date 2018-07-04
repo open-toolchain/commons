ab1feddb30736b78fb73489bd44ae67629f3d082
Philippe Mulet <phmulet@philippes-mbp.pornichet.fr.ibm.com>
2018-07-04 13:59:43 +0200

    infer from img url

--- scripts/check_vulnerabilities.sh
+++ scripts/check_vulnerabilities.sh
@@ -8,11 +8,6 @@
 # ------------------
 # source: https://raw.githubusercontent.com/open-toolchain/commons/master/scripts/check_vulnerabilities.sh
 # Input env variables (can be received via a pipeline environment properties.file.
-echo "IMAGE_NAME=${IMAGE_NAME}"
-echo "IMAGE_TAG=${IMAGE_TAG}"
-echo "BUILD_NUMBER=${BUILD_NUMBER}"
-echo "REGISTRY_URL=${REGISTRY_URL}"
-echo "REGISTRY_NAMESPACE=${REGISTRY_NAMESPACE}"
 
 # View build properties
 if [ -f build.properties ]; then 
@@ -21,15 +16,28 @@ if [ -f build.properties ]; then
 else 
   echo "build.properties : not found"
 fi 
+
+# If running after build_image.sh in same stage, reuse the exported variable PIPELINE_IMAGE_URL
+if [[ -z PIPELINE_IMAGE_URL ]]; then
+  PIPELINE_IMAGE_URL=${REGISTRY_URL}/${REGISTRY_NAMESPACE}/${IMAGE_NAME}:${IMAGE_TAG}
+else
+  # extract from img url
+  REGISTRY_URL=$(echo ${PIPELINE_IMAGE_URL} | cut -f1 -d/)
+  REGISTRY_NAMESPACE=$(echo ${PIPELINE_IMAGE_URL} | cut -f2 -d/)
+  IMAGE_NAME=$(echo ${PIPELINE_IMAGE_URL} | cut -f3 -d/ | cut -f1 -d:)
+  IMAGE_TAG=$(echo ${PIPELINE_IMAGE_URL} | cut -f3 -d/ | cut -f2 -d:)
+fi
+echo "PIPELINE_IMAGE_URL=${PIPELINE_IMAGE_URL}"
+echo "REGISTRY_URL=${REGISTRY_URL}"
+echo "REGISTRY_NAMESPACE=${REGISTRY_NAMESPACE}"
+echo "IMAGE_NAME=${IMAGE_NAME}"
+echo "IMAGE_TAG=${IMAGE_TAG}"
+
 # also run 'env' command to find all available env variables
 # or learn more about the available environment variables at:
 # https://console.bluemix.net/docs/services/ContinuousDelivery/pipeline_deploy_var.html#deliverypipeline_environment
 
 bx cr images --restrict ${REGISTRY_NAMESPACE}/${IMAGE_NAME}
-# If running after build_image.sh in same stage, reuse the exported variable PIPELINE_IMAGE_URL
-if [[ -z PIPELINE_IMAGE_URL ]]; then
-  PIPELINE_IMAGE_URL=${REGISTRY_URL}/${REGISTRY_NAMESPACE}/${IMAGE_NAME}:${IMAGE_TAG}
-fi
 echo -e "Checking vulnerabilities in image: ${PIPELINE_IMAGE_URL}"
 for ITER in {1..30}
 do
