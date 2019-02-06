17e20e825722e63cc8edd055c03390b1b4d1eaf1
hmagph <mulet.philippe@gmail.com>
2019-02-06 23:20:07 +0100

    fix idra install

--- scripts/check_publish_gate_component_helm_chart.sh
+++ scripts/check_publish_gate_component_helm_chart.sh
@@ -29,6 +29,8 @@ ls -l
 
 # Install DRA CLI
 export PATH=/opt/IBM/node-v4.2/bin:$PATH
+npm install -g npm@3.7.2 ### work around default npm 2.1.1 instability
+npm install
 npm install -g grunt-idra3
 
 # Evaluate the gate against the version matching the git commit
--- scripts/deploy_umbrella_chart.sh
+++ scripts/deploy_umbrella_chart.sh
@@ -81,6 +81,8 @@ if [[ ! -d ./insights ]]; then
 else
   # Install DRA CLI
   export PATH=/opt/IBM/node-v4.2/bin:$PATH
+  npm install -g npm@3.7.2 ### work around default npm 2.1.1 instability
+  npm install
   npm install -g grunt-idra3
 
   # get the deployment result from helm status command
