da955b61a401cc586923c4836a149e3706b7fa94
Philippe Mulet <hmagph@users.noreply.github.com>
2019-03-03 13:02:20 +0100

    Update check_health.sh

--- scripts/check_health.sh
+++ scripts/check_health.sh
@@ -20,10 +20,10 @@ IMAGE_REPOSITORY=${REGISTRY_URL}/${REGISTRY_NAMESPACE}/${IMAGE_NAME}
 IP_ADDR=$(bx cs workers ${PIPELINE_KUBERNETES_CLUSTER_NAME} | grep normal | head -n 1 | awk '{ print $2 }')
 
 CONTAINERS_JSON=$(kubectl get deployments --namespace ${CLUSTER_NAMESPACE} -o json | jq -r ".items[].spec.template.spec.containers[]? | select(.image==\"${IMAGE_REPOSITORY}:${IMAGE_TAG}\") ")
-echo $CONTAINERS_JSON
+echo $CONTAINERS_JSON | jq .
 
-LIVENESS_PROBE_PATH=$(echo $CONTAINERS_JSON | jq -r ".livenessProbe.httpGet.path" | head -n 1)
-LIVENESS_PROBE_PORT=$(echo $CONTAINERS_JSON | jq -r ".livenessProbe.httpGet.port" | head -n 1)
+LIVENESS_PROBE_PATH=$(echo $CONTAINERS_JSON | jq -r ".livenessProbe?.httpGet.path" | head -n 1)
+LIVENESS_PROBE_PORT=$(echo $CONTAINERS_JSON | jq -r ".livenessProbe?.httpGet.port" | head -n 1)
 if [ ! -z "${LIVENESS_PROBE_PATH}" ]; then
   LIVENESS_PROBE_URL=http://${IP_ADDR}/${LIVENESS_PROBE_PATH}:${LIVENESS_PROBE_PATH}
   if [ "$(curl -Is ${LIVENESS_PROBE_PATH} --connect-timeout 3 --max-time 5 --retry 2 --retry-max-time 30 | head -n 1 | grep 200)" != "" ]; then
@@ -37,8 +37,8 @@ else
   echo "No liveness probe endpoint defined (should be specified in deployment resource."
 fi
 
-READINESS_PROBE_PATH=$(echo $CONTAINERS_JSON | jq -r ".readinessProbe.httpGet.path" | head -n 1)
-READINESS_PROBE_PORT=$(echo $CONTAINERS_JSON | jq -r ".readinessProbe.httpGet.port" | head -n 1)
+READINESS_PROBE_PATH=$(echo $CONTAINERS_JSON | jq -r ".readinessProbe?.httpGet.path" | head -n 1)
+READINESS_PROBE_PORT=$(echo $CONTAINERS_JSON | jq -r ".readinessProbe?.httpGet.port" | head -n 1)
 if [ ! -z "${READINESS_PROBE_PATH}" ]; then
   READINESS_PROBE_URL=http://${IP_ADDR}/${LIVENESS_PROBE_PATH}:${LIVENESS_PROBE_PATH}
   if [ "$(curl -Is ${READINESS_PROBE_URL} --connect-timeout 3 --max-time 5 --retry 2 --retry-max-time 30 | head -n 1 | grep 200)" != "" ]; then
