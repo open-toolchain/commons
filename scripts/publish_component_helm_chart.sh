efdaebe0850be79ae333100c196aa155761e9691
hmagph <mulet.philippe@gmail.com>
2019-06-16 22:16:55 +0200

    switch branch hack

--- scripts/publish_component_helm_chart.sh
+++ scripts/publish_component_helm_chart.sh
@@ -119,7 +119,7 @@ for ITER in {1..30}
 do
   echo "Fetching umbrella repo"
   git clone ${UMBRELLA_ACCESS_REPO_URL}
-  git checkout -b hack
+  git checkout hack ################## HACK
   cd ${UMBRELLA_REPO_NAME}
   ls -al
   echo "Remove previous component data"
