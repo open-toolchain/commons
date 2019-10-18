578637a54c00449ac9118e70367d4543f88dddb6
Benoit Jaunin <jaunin.b@fr.ibm.com>
2019-10-18 18:59:12 +0200

    Remove numeric check for PORT

--- scripts/check_and_deploy_kubectl.sh
+++ scripts/check_and_deploy_kubectl.sh
@@ -143,12 +143,7 @@ EOT
 )
   # Find the port
   PORT=$(ibmcloud cr image-inspect "${REGISTRY_URL}/${REGISTRY_NAMESPACE}/${IMAGE_NAME}:${IMAGE_TAG}" --format '{{ range $key,$value := .ContainerConfig.ExposedPorts }} {{ $key }} {{ "" }} {{end}}' | sed -E 's/^[^0-9]*([0-9]+).*$/\1/')
-  if [ "$PORT" -eq "$PORT" ] 2>/dev/null; then
-    echo "ExposedPort $PORT found while inspecting image ${REGISTRY_URL}/${REGISTRY_NAMESPACE}/${IMAGE_NAME}:${IMAGE_TAG}"
-  else 
-    echo "Found '$PORT' as ExposedPort while inspecting image ${REGISTRY_URL}/${REGISTRY_NAMESPACE}/${IMAGE_NAME}:${IMAGE_TAG}, non numeric value so using 5000 as containerPort"
-    PORT=5000
-  fi
+  echo "ExposedPort '$PORT' found while inspecting image ${REGISTRY_URL}/${REGISTRY_NAMESPACE}/${IMAGE_NAME}:${IMAGE_TAG}"
   # Generate deployment file  
   echo "GENERATED ${DEPLOYMENT_FILE}:"
   printf "$deployment_content" \
