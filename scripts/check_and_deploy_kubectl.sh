28486f6ff29d2e7312b3282aca57d9f99ca91432
Benoit Jaunin <jaunin.b@fr.ibm.com>
2019-10-31 15:21:18 +0100

    Reduce noise in log

--- scripts/check_and_deploy_kubectl.sh
+++ scripts/check_and_deploy_kubectl.sh
@@ -255,7 +255,7 @@ if [ ! -z "${APP_SERVICE}" ]; then
     echo "Using first worker node ip address as NodeIP: ${IP_ADDR}"
   else 
     # check if a route resource exists in the this kubernetes cluster
-    if kubectl explain route; then
+    if kubectl explain route > /dev/null 2>&1; then
       # Assuming the kubernetes target cluster is an openshift cluster
       # Check if a route exists for exposing the service ${APP_SERVICE}
       if  kubectl get routes --namespace ${CLUSTER_NAMESPACE} -o json | jq --arg service "$APP_SERVICE" -e '.items[] | select(.spec.to.name==$service)'; then
