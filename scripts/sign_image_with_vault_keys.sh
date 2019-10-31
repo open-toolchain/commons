ecc64f0e86586120398a5f40a33b5a755015f44d
Padraic Edwards <padraic.edwards@ie.ibm.com>
2019-10-31 15:06:39 +0000

    Add sign image with vault keys script

---
+++ scripts/sign_image_with_vault_keys.sh
@@ -0,0 +1,27 @@
+
+#!/bin/bash
+#Requires
+#VAULT_INSTANCE -name of vault
+# $IBMCLOUD_TARGET_REGION -region hosting Key Protect Vault instance
+# $IBMCLOUD_TARGET_RESOURCE_GROUP of the Key Protect Vault Instance
+# REGISTRY_NAMESPACE namespace of registry
+# $IMAGE_NAME
+# $IMAGE_TAG
+# $DEVOPS_SIGNER
+export DOCKER_CONTENT_TRUST=1
+echo "Vault instance $VAULT_INSTANCE used to retrieve signing keys"
+source <(curl -sSL "https://raw.githubusercontent.com/jauninb/jumpstart/master/signatures.sh")
+# Restore signer pem key
+VAULT_DATA=$(buildVaultAccessDetailsJSON "$VAULT_INSTANCE" "$IBMCLOUD_TARGET_REGION" "$IBMCLOUD_TARGET_RESOURCE_GROUP")
+JSON_DATA="$(readData "$REGISTRY_NAMESPACE.keys" "$VAULT_DATA")"
+signerkey=$(getJSONValue "$DEVOPS_SIGNER" "$JSON_DATA")
+writeFile "$signerkey"
+# Retrieve the signer passphrase
+export DOCKER_CONTENT_TRUST_REPOSITORY_PASSPHRASE=$(getJSONValue "passphrase" "$signerkey")
+
+export DCT_DISABLED=false
+# Pull the image
+docker pull "$REGISTRY_URL/$REGISTRY_NAMESPACE/$IMAGE_NAME:$IMAGE_TAG"
+# Sign the image
+docker trust sign "$REGISTRY_URL/$REGISTRY_NAMESPACE/$IMAGE_NAME:$IMAGE_TAG"
+docker trust inspect --pretty "$REGISTRY_URL/$REGISTRY_NAMESPACE/$IMAGE_NAME"
