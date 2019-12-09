5e274c439ab2fdbc07fcee6a81e8a427b9ea820c
hmagph <mulet.philippe@gmail.com>
2019-12-09 17:17:52 +0100

    skip if no APP_URL

--- scripts/check_health.sh
+++ scripts/check_health.sh
@@ -16,6 +16,11 @@ echo "REGISTRY_NAMESPACE=${REGISTRY_NAMESPACE}"
 echo "CLUSTER_NAMESPACE=${CLUSTER_NAMESPACE}"
 echo "APP_URL=${APP_URL}"
 
+if [ -z "${APP_URL}" ]; then
+  echo "APP_URL env variable not set. Skipping health check !"
+  exit 0
+fi
+
 # If custom cluster credentials available, connect to this cluster instead
 if [ ! -z "${KUBERNETES_MASTER_ADDRESS}" ]; then
   kubectl config set-cluster custom-cluster --server=https://${KUBERNETES_MASTER_ADDRESS}:${KUBERNETES_MASTER_PORT} --insecure-skip-tls-verify=true
