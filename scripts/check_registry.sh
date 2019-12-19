66c0df7272d615ab9bfeec82d974301122e22ec7
Benoit Jaunin <jaunin.b@fr.ibm.com>
2019-12-19 12:41:23 +0100

    Ensure non failing ibmcloud cr plan & quota (#50)
    https://github.ibm.com/org-ids/roadmap/issues/11249

--- scripts/check_registry.sh
+++ scripts/check_registry.sh
@@ -31,8 +31,8 @@ fi
 
 echo "=========================================================="
 echo "Checking registry current plan and quota"
-ibmcloud cr plan
-ibmcloud cr quota
+ibmcloud cr plan || true
+ibmcloud cr quota || true
 echo "If needed, discard older images using: ibmcloud cr image-rm"
 echo "Checking registry namespace: ${REGISTRY_NAMESPACE}"
 NS=$( ibmcloud cr namespaces | grep ${REGISTRY_NAMESPACE} ||: )
