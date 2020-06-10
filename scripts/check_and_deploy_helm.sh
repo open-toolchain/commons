88229ee2308202815672c57b96290494db99d708
Jerome Lanneluc <jerome_lanneluc@fr.ibm.com>
2020-06-10 16:52:32 +0200

    handle case of several clusters with same name

--- scripts/check_and_deploy_helm.sh
+++ scripts/check_and_deploy_helm.sh
@@ -82,6 +82,11 @@ echo "CHECKING CLUSTER readiness and namespace existence"
 if [ -z "${KUBERNETES_MASTER_ADDRESS}" ]; then
   IP_ADDR=$( ibmcloud ks workers --cluster ${PIPELINE_KUBERNETES_CLUSTER_NAME} | grep normal | head -n 1 | awk '{ print $2 }' )
   if [ -z "${IP_ADDR}" ]; then
+    # If several clusters have same name, the above now fails, so switch to using the cluster id
+    CLUSTER_ID=$( ibmcloud ks cluster ls | grep ${PIPELINE_KUBERNETES_CLUSTER_NAME} | head -n 1 | awk '{ print $2 }' )
+    IP_ADDR=$( ibmcloud ks workers --cluster ${CLUSTER_ID} | grep normal | head -n 1 | awk '{ print $2 }' )
+  fi
+  if [ -z "${IP_ADDR}" ]; then
     echo -e "${PIPELINE_KUBERNETES_CLUSTER_NAME} not created or workers not ready"
     exit 1
   fi
