b73c2603820dabafb38500a2331246c264d61dca
Jerome Lanneluc <jerome_lanneluc@fr.ibm.com>
2020-06-12 14:37:02 +0200

    debug

--- scripts/check_and_deploy_helm.sh
+++ scripts/check_and_deploy_helm.sh
@@ -81,6 +81,7 @@ echo "=========================================================="
 echo "CHECKING CLUSTER readiness and namespace existence"
 if [ -z "${KUBERNETES_MASTER_ADDRESS}" ]; then
   CLUSTER_ID=${PIPELINE_KUBERNETES_CLUSTER_ID:-${PIPELINE_KUBERNETES_CLUSTER_NAME}} # use cluster id instead of cluster name to handle case where there are multiple clusters with same name
+echo "CLUSTER_ID=$CLUSTER_ID"
   IP_ADDR=$( ibmcloud ks workers --cluster ${CLUSTER_ID} | grep normal | head -n 1 | awk '{ print $2 }' )
   if [ -z "${IP_ADDR}" ]; then
     echo -e "${PIPELINE_KUBERNETES_CLUSTER_NAME} not created or workers not ready"
