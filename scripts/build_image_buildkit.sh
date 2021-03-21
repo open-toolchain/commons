a143e6f87464f1e3399111dcec2509ed46cd7860
Hua Yuen <huayuenh@ie.ibm.com>
2021-03-21 20:15:35 +0000

    add digest

--- scripts/build_image_buildkit.sh
+++ scripts/build_image_buildkit.sh
@@ -86,12 +86,13 @@ else
   done
 fi
 set -x
-buildctl build \
+LOGS=$(buildctl build \
     --frontend dockerfile.v0 --opt filename=${DOCKER_FILE} --local dockerfile=${DOCKER_ROOT} \
     ${BUILD_ARGS} --local context=${DOCKER_ROOT} \
-    --import-cache type=registry,ref=${REGISTRY_URL}/${REGISTRY_NAMESPACE}/${IMAGE_NAME} 2>&1 | tee $ARCHIVE_DIR/build.log \
-    --output type=image,name="${REGISTRY_URL}/${REGISTRY_NAMESPACE}/${IMAGE_NAME}:${IMAGE_TAG}",push=true
+    --import-cache type=registry,ref=${REGISTRY_URL}/${REGISTRY_NAMESPACE}/${IMAGE_NAME} \
+    --output type=image,name="${REGISTRY_URL}/${REGISTRY_NAMESPACE}/${IMAGE_NAME}:${IMAGE_TAG}",push=true) > $ARCHIVE_DIR/build.logs
 set +x
+echo $ARCHIVE_DIR/build.logs
 
 ibmcloud cr image-inspect ${REGISTRY_URL}/${REGISTRY_NAMESPACE}/${IMAGE_NAME}:${IMAGE_TAG}
 DIGEST=$(ibmcloud cr image-inspect --format "{{ .ID }}" ${REGISTRY_URL}/${REGISTRY_NAMESPACE}/${IMAGE_NAME}:${IMAGE_TAG})
