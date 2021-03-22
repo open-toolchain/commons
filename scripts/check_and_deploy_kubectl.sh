3702ba45d36ad568c4dfc86ec98af9f272d5edcc
Benoit Jaunin <jaunin.b@fr.ibm.com>
2021-03-22 21:16:01 +0100

    Update check_and_deploy_kubectl.sh

--- scripts/check_and_deploy_kubectl.sh
+++ scripts/check_and_deploy_kubectl.sh
@@ -295,12 +295,12 @@ echo "=========================================================="
 echo "DEPLOYMENT SUCCEEDED"
 APP_URL=""
 if [ "${CLUSTER_INGRESS_SUBDOMAIN}" ] && [ "${USE_ISTIO_GATEWAY}" != true ]; then
-  APP_INGRESS=$(kubectl get ingress --namespace "$CLUSTER_NAMESPACE" -o json | jq -r --arg service_name "${APP_SERVICE}" ' .items[] | first(select(.spec.rules[].http.paths[].backend.serviceName==$service_name)) | .metadata.name')
+  APP_INGRESS=$(kubectl get ingress --namespace "$CLUSTER_NAMESPACE" -o json | jq -r --arg service_name "${APP_SERVICE}" ' .items[] | first(select(.spec.rules[].http.paths[].backend.serviceName==$service_name or .spec.rules[].http.paths[].backend.service.name==$service_name)) | .metadata.name')
   if [ "$APP_INGRESS" ]; then
     INGRESS_JSON=$(kubectl get ingress --namespace "$CLUSTER_NAMESPACE" "${APP_INGRESS}" -o json)
     # Expose app using ingress host and path for the service
-    APP_HOST=$(echo $INGRESS_JSON | jq -r --arg service_name "$APP_SERVICE" '.spec.rules[] | first(select(.http.paths[].backend.serviceName==$service_name)) | .host' | head -n1)
-    APP_PATH=$(echo $INGRESS_JSON | jq -r --arg service_name "$APP_SERVICE" '.spec.rules[].http.paths[] | first(select(.backend.serviceName==$service_name)) | .path' | head -n1)
+    APP_HOST=$(echo $INGRESS_JSON | jq -r --arg service_name "$APP_SERVICE" '.spec.rules[] | first(select(.http.paths[].backend.serviceName==$service_name or .http.paths[].backend.service.name==$service_name)) | .host' | head -n1)
+    APP_PATH=$(echo $INGRESS_JSON | jq -r --arg service_name "$APP_SERVICE" '.spec.rules[].http.paths[] | first(select(.backend.serviceName==$service_name or .backend.service.name==$service_name)) | .path' | head -n1)
     # Remove any group in the path in case of regex in ingress path definition
     # https://kubernetes.github.io/ingress-nginx/user-guide/ingress-path-matching/
     APP_PATH=$(echo "$APP_PATH" | sed "s/([^)]*)//g")
